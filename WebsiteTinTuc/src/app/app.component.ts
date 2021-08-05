0bdc6beb4313cd9560b01e5840b6c9a1c6d05865
nhattan2000 <18130208@st.hcmuaf.edu.vn>
2021-08-05 20:14:53 +0700

    them chuc nang len dau trang

--- WebsiteTinTuc/src/app/app.component.ts
+++ WebsiteTinTuc/src/app/app.component.ts
@@ -1,14 +1,29 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
+import {HomeComponent} from "./home/home.component";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent  {
   title = 'WebsiteTinTuc';
-  constructor(public route: ActivatedRoute) {
-
+  constructor() {
+    setInterval(this.scrollFunction,100);
   }
+
+  onActivate(event:Event){
+    window.scroll(0,0);
+}
+  scrollFunction(){
+
+    let element :HTMLElement = document.getElementById('btn-backontop')!;
+    if(document.body.scrollTop>100 || document.documentElement.scrollTop >100){
+      element.style.display = 'block';
+    }else{
+      element.style.display = 'none';
+    }
+  }
+
 }
