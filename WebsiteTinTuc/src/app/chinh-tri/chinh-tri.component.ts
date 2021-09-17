034ffb5b590554c7c085ab8113d77fc94cd8229a
vietprao2020 <deptraiphaithe258@gmail.com>
2021-09-17 21:31:13 +0700

    update lastest

--- WebsiteTinTuc/src/app/chinh-tri/chinh-tri.component.ts
+++ WebsiteTinTuc/src/app/chinh-tri/chinh-tri.component.ts
@@ -15,19 +15,21 @@ export class ChinhTriComponent implements OnInit {
     private https: HttpClient,
     private newrssservice: NewRssService,
     private newrssservicedetail: NewRssDetailService,
-    private testsearchservice: TestSearchService,
-
+    private testsearchservice: TestSearchService
   ) {}
   RssDataChinhTri: Rss | any;
+
   ngOnInit(): void {
     this.GetRssFeedDataChinhTri();
   }
+
   GetRssFeedDataChinhTri() {
     this.newrssservice.GetRssChinhTri().subscribe((data) => {
       var options = {
         mergeAttrs: true,
         tagNameProcessors: [xml2js.processors.stripPrefix],
       };
+
       let parseString = xml2js.parseString;
       parseString(data, options, (err, result: Rss) => {
         this.RssDataChinhTri = result;
@@ -39,18 +41,22 @@ export class ChinhTriComponent implements OnInit {
   getRssDetail(index: number) {
     this.newrssservicedetail.index = index;
   }
-  countHours(i:number){
-      var datenews = new Date(this.RssDataChinhTri?.rss.channel[0].item[i].pubDate);
-      var currentDate = new Date(Date.now());
-      if(datenews.getDate()==currentDate.getDate()&& datenews.getMonth()==currentDate.getMonth()&& datenews.getFullYear()==currentDate.getFullYear()){
-        if(datenews.getHours()!=currentDate.getHours())
-        return currentDate.getHours()-datenews.getHours()+" giờ trước";
-        else{
-          return currentDate.getMinutes()-datenews.getMonth()+" phút trước";
-        }
+  countHours(i: number) {
+    var datenews = new Date(
+      this.RssDataChinhTri?.rss.channel[0].item[i].pubDate
+    );
+    var currentDate = new Date(Date.now());
+    if (
+      datenews.getDate() == currentDate.getDate() &&
+      datenews.getMonth() == currentDate.getMonth() &&
+      datenews.getFullYear() == currentDate.getFullYear()
+    ) {
+      if (datenews.getHours() != currentDate.getHours())
+        return currentDate.getHours() - datenews.getHours() + ' giờ trước';
+      else {
+        return currentDate.getMinutes() - datenews.getMonth() + ' phút trước';
       }
-      return 0;
-
+    }
+    return 0;
   }
 }
-
